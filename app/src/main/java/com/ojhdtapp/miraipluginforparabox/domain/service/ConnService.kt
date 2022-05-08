f1758d7cbfdd827de30b10bf7ffe3b12443cb8e1
ojhdt <ojhdtmail@gmail.com>
2022-05-08 17:58:51 +0800

    conn service

---
+++ app/src/main/java/com/ojhdtapp/miraipluginforparabox/domain/service/ConnService.kt
@@ -0,0 +1,45 @@
+package com.ojhdtapp.miraipluginforparabox.domain.service
+
+import android.app.Service
+import android.content.Intent
+import android.os.*
+import android.util.Log
+import androidx.lifecycle.LifecycleService
+
+class ConnService : LifecycleService() {
+    private lateinit var sMessenger: Messenger
+    private lateinit var cMessenger: Messenger
+
+    override fun onCreate() {
+        super.onCreate()
+        Log.d("parabox", "service created")
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        Log.d("parabox", "service started")
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    override fun onBind(intent: Intent): IBinder {
+        super.onBind(intent)
+        sMessenger = Messenger(ConnHandler())
+        return sMessenger.binder
+    }
+
+    inner class ConnHandler : Handler() {
+        override fun handleMessage(msg: Message) {
+            super.handleMessage(msg)
+            cMessenger = msg.replyTo
+            val str = (msg.obj as Bundle).getString("str") ?: "error"
+            try {
+                cMessenger.send(Message().apply {
+                    obj = Bundle().apply {
+                        putString("str", "$str--connected!")
+                    }
+                })
+            } catch (e: RemoteException) {
+                e.printStackTrace()
+            }
+        }
+    }
+}
