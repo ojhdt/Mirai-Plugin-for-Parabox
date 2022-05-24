511fc5ee8b196ad2b889e75e2f6be431e2cf9795
ojhdt <ojhdtmail@gmail.com>
2022-05-24 11:23:30 +0800

    Update SharedAxisZTransition.kt

--- app/src/main/java/com/ojhdtapp/miraipluginforparabox/ui/util/SharedAxisZTransition.kt
+++ app/src/main/java/com/ojhdtapp/miraipluginforparabox/ui/util/SharedAxisZTransition.kt
@@ -2,36 +2,26 @@ package com.ojhdtapp.miraipluginforparabox.ui.util
 
 import androidx.compose.animation.*
 import androidx.compose.animation.core.tween
+import androidx.compose.material3.MaterialTheme
 import androidx.navigation.NavBackStackEntry
 import com.ramcosta.composedestinations.spec.DestinationStyle
+import com.ramcosta.composedestinations.spec.DestinationStyle.Animated.None.enterTransition
 
 @OptIn(ExperimentalAnimationApi::class)
 object SharedAxisZTransition : DestinationStyle.Animated {
     override fun AnimatedContentScope<NavBackStackEntry>.enterTransition(): EnterTransition? {
-        return slideInHorizontally(
-            initialOffsetX = { 1000 },
-            animationSpec = tween(700)
-        )
+        return scaleIn(tween(200), 0.9f) + fadeIn(tween(200))
     }
 
     override fun AnimatedContentScope<NavBackStackEntry>.exitTransition(): ExitTransition? {
-        return slideOutHorizontally(
-            targetOffsetX = { -1000 },
-            animationSpec = tween(700)
-        )
+        return scaleOut(tween(200), 1.1f) + fadeOut(tween(200))
     }
 
     override fun AnimatedContentScope<NavBackStackEntry>.popEnterTransition(): EnterTransition? {
-        return slideInHorizontally(
-            initialOffsetX = { -1000 },
-            animationSpec = tween(700)
-        )
+        return scaleIn(tween(200), 1.1f) + fadeIn(tween(200))
     }
 
     override fun AnimatedContentScope<NavBackStackEntry>.popExitTransition(): ExitTransition? {
-        return slideOutHorizontally(
-            targetOffsetX = { 1000 },
-            animationSpec = tween(700)
-        )
+        return scaleOut(tween(200), 0.9f) + fadeOut(tween(200))
     }
 }
