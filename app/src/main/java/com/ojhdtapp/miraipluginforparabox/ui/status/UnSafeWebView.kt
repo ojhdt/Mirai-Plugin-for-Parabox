86c53f20ca67fc9545d8528641b17be99e21451d
ojhdt <ojhdtmail@gmail.com>
2023-02-20 18:32:41 +0800

    adjust height of webview

--- app/src/main/java/com/ojhdtapp/miraipluginforparabox/ui/status/UnSafeWebView.kt
+++ app/src/main/java/com/ojhdtapp/miraipluginforparabox/ui/status/UnSafeWebView.kt
@@ -1,19 +1,23 @@
 package com.ojhdtapp.miraipluginforparabox.ui.status
 
+import android.net.Uri
 import android.util.Log
 import android.webkit.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.key.Key.Companion.U
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.google.gson.Gson
 import com.google.gson.JsonElement
+import com.google.gson.JsonParser
 import com.google.gson.reflect.TypeToken
-import com.ojhdtapp.miraipluginforparabox.domain.util.GsonParser
-import com.ojhdtapp.miraipluginforparabox.domain.util.JsonParser
+import kotlinx.serialization.json.Json
 import javax.inject.Inject
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun UnSafeWebView(modifier: Modifier = Modifier, url: String, onConfirm : (res : String) -> Unit) {
     val viewModel: StatusPageViewModel = hiltViewModel()
@@ -21,50 +25,52 @@ fun UnSafeWebView(modifier: Modifier = Modifier, url: String, onConfirm : (res :
         WebView.setWebContentsDebuggingEnabled(true)
         WebView(context).apply {
             webViewClient = object : WebViewClient() {
-                override fun onPageFinished(view: WebView?, url: String?) {
-                    super.onPageFinished(view, url)
-                    view?.evaluateJavascript(
-                        """
-                        mqq.invoke = function(a,b,c){ return bridge.invoke(a,b,JSON.stringify(c))}
-                        """.trimIndent()
-                    ) {}
-                }
-            }
-            webChromeClient = object : WebChromeClient() {
-                override fun onConsoleMessage(consoleMessage: ConsoleMessage?): Boolean {
-                    val msg = consoleMessage?.message()
-                    // 按下回到qq按钮之后会打印这句话，于是就用这个解决了。。。。
-                    if (msg?.startsWith("手Q扫码验证") == true) {
-                        onConfirm("success")
+                override fun shouldOverrideUrlLoading(
+                    view: WebView,
+                    request: WebResourceRequest
+                ): Boolean = onJsBridgeInvoke(request.url)
+
+                @Deprecated("Deprecated in Java")
+                override fun shouldOverrideUrlLoading(
+                    view: WebView,
+                    url: String
+                ): Boolean = onJsBridgeInvoke(Uri.parse(url))
+
+                fun onJsBridgeInvoke(request: Uri): Boolean {
+                    if (request.path.equals("/onVerifyCAPTCHA")) {
+                        val p = request.getQueryParameter("p") ?: return false
+                        val jsData = JsonParser.parseString(p).asJsonObject
+                        onConfirm(jsData["ticket"].asString)
                     }
-                    return super.onConsoleMessage(consoleMessage)
+                    return false
                 }
             }
             settings.apply {
                 javaScriptEnabled = true
                 domStorageEnabled = true
             }
-            addJavascriptInterface(Bridge(GsonParser()) {
-                onConfirm(it)
-            }, "bridge")
             loadUrl(url)
         }
-    })
+    },
+    modifier = modifier
+    )
 }
 
-class Bridge(
-    private val parser: JsonParser,
-    val onFinished: (ticket: String) -> Unit
-) {
-    @JavascriptInterface
-    fun invoke(cls: String?, method: String?, data: String?) {
-        if (data != null) {
-            val obj = parser.fromJson<JsonElement>(data, object : TypeToken<JsonElement>() {}.type)
-            if (method == "onVerifyCAPTCHA") {
-                obj?.let {
-                    onFinished(it.asJsonObject["ticket"].asString)
-                }
-            }
-        }
-    }
-}
+
+
+//class Bridge(
+//    private val parser: JsonParser,
+//    val onFinished: (ticket: String) -> Unit
+//) {
+//    @JavascriptInterface
+//    fun invoke(cls: String?, method: String?, data: String?) {
+//        if (data != null) {
+//            val obj = parser.fromJson<JsonElement>(data, object : TypeToken<JsonElement>() {}.type)
+//            if (method == "onVerifyCAPTCHA") {
+//                obj?.let {
+//                    onFinished(it.asJsonObject["ticket"].asString)
+//                }
+//            }
+//        }
+//    }
+//}
